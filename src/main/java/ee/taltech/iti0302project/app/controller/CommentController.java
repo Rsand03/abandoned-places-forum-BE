ac677c76f5745d288ec497d165091a07c8f21dc1
martin <mjanov@taltech.ee>
2024-11-12 02:12:02 +0200

    save

---
+++ src/main/java/ee/taltech/iti0302project/app/controller/CommentController.java
@@ -0,0 +1,37 @@
+package ee.taltech.iti0302project.app.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import ee.taltech.iti0302project.app.dto.feed.CommentDto;
+import ee.taltech.iti0302project.app.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/feed/{postId}/comments")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<CommentDto> createComment(
+            @PathVariable Long postId,
+            @RequestBody() JsonNode body,
+            @RequestHeader("User-ID") UUID userId
+    ) {
+        String comment = body.get("comment").asText();
+        CommentDto commentDto = commentService.createComment(postId, comment, userId);
+        System.out.println(comment);
+        return ResponseEntity.ok(commentDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CommentDto>> getCommentsByPostId(@PathVariable Integer postId) {
+        List<CommentDto> comments = commentService.getCommentsByPostId(postId);
+        return ResponseEntity.ok(comments);
+    }
+}
---
+++ src/main/java/ee/taltech/iti0302project/app/dto/mapper/feed/CommentMapper.java
@@ -0,0 +1,25 @@
+package ee.taltech.iti0302project.app.dto.mapper.feed;
+
+import ee.taltech.iti0302project.app.dto.feed.CommentDto;
+import ee.taltech.iti0302project.app.entity.feed.CommentEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface CommentMapper {
+
+    CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
+
+    @Mapping(source = "createdBy.username", target = "createdByUsername")
+    @Mapping(source = "body", target = "comment")
+    CommentDto toDto(CommentEntity commentEntity);
+
+    @Mapping(source = "createdBy.username", target = "createdByUsername")
+    List<CommentDto> toDtoList(List<CommentEntity> commentEntityList);
+
+    @Mapping(source = "comment", target = "body")
+    CommentEntity toEntity(CommentDto commentDto);
+}
