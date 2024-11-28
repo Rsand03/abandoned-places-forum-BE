ad3eea8d60214a4637183846519852d300811f76
MathiasKaasma <106476093+MathiasKaasma@users.noreply.github.com>
2024-11-28 21:48:32 +0200

    Update dto and entity structure

--- src/main/java/ee/taltech/iti0302project/app/dto/location/LocationBookmarkCreateDto.java
+++ src/main/java/ee/taltech/iti0302project/app/dto/location/LocationBookmarkCreateDto.java
@@ -9,11 +9,11 @@ import java.util.UUID;
 public class LocationBookmarkCreateDto {
 
     @NotNull
-    @Size(max = 25)
+    @Size(max = 20)
     private String type;
 
     @NotNull
-    private UUID createdBy;
+    private UUID createdByUserUuid;
 
     @NotNull
     private UUID locationId;
--- src/main/java/ee/taltech/iti0302project/app/dto/mapper/location/LocationBookmarkMapper.java
+++ src/main/java/ee/taltech/iti0302project/app/dto/mapper/location/LocationBookmarkMapper.java
@@ -1,4 +1,18 @@
 package ee.taltech.iti0302project.app.dto.mapper.location;
 
+import ee.taltech.iti0302project.app.dto.location.LocationBookmarkCreateDto;
+import ee.taltech.iti0302project.app.dto.location.LocationBookmarkDto;
+import ee.taltech.iti0302project.app.entity.location.LocationBookmarkEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface LocationBookmarkMapper {
+    @Mapping(source = "createdByUserUuid", target = "createdBy")
+    @Mapping(source = "locationId", target = "locationId")
+    LocationBookmarkEntity toEntity(LocationBookmarkCreateDto locationBookmarkCreateDto);
+
+    @Mapping(source = "createdBy", target = "createdByUserUuid")
+    LocationBookmarkDto toResponseDto(LocationBookmarkEntity locationBookmarkEntity);
 }
