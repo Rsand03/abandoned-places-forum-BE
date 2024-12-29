d9161617e63359493106f6a556653b4b9b6d95a5
rasand <rasand@taltech.ee>
2024-12-30 01:24:41 +0200

    Added integration tests with auth + jwt parsing; liquibase refacto

--- src/test/java/ee/taltech/iti0302project/test/controller/location/LocationControllerTest.java
+++ src/test/java/ee/taltech/iti0302project/test/controller/location/LocationControllerTest.java
@@ -1,5 +1,8 @@
 package ee.taltech.iti0302project.test.controller.location;
 
+import ee.taltech.iti0302project.app.dto.auth.UserLoginDto;
+import ee.taltech.iti0302project.app.service.auth.AuthService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,19 +22,46 @@ class LocationControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private AuthService authService;
+
+    private String userUserAuthToken;
+
+    @BeforeEach
+    void setup() {
+        UserLoginDto dto = new UserLoginDto();
+        dto.setUsername("user");
+        dto.setPassword("user");
+        userUserAuthToken = authService.authenticateUser(dto).getToken();
+    }
+
     @Test
-    void getLocationAttributes() {
+    void getLocationAttributes() throws Exception {
+        mvc.perform(get("/api/locations/attributes").with(user("user")))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.categories.length()").value(14))
+                .andExpect(jsonPath("$.conditions.length()").value(6))
+                .andExpect(jsonPath("$.statuses.length()").value(8))
+                .andExpect(jsonPath("$.categories[0].name").value("Määramata"))
+                .andExpect(jsonPath("$.conditions[5].name").value("Väga hea"))
+                .andExpect(jsonPath("$.statuses[7].name").value("Omaniku loal ligipääsetav"));
     }
 
     @Test
     void getLocationStatuses() throws Exception {
         mvc.perform(get("/api/locations/statuses").with(user("user")))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].name").value("Lammutatud"));
+                .andExpect(jsonPath("$[1].name").value("Lammutatud"));
     }
 
     @Test
-    void getFilteredLocations() {
+    void getFilteredLocations() throws Exception {
+        mvc.perform((get("/api/locations")
+                        .header("Authorization", "Bearer " + userUserAuthToken)
+                        .param("mainCategoryId", "1")))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value("a59b74f9-d7fc-4c8e-bf47-2b060276421e"))
+                .andExpect(jsonPath("$.length()").value(1));
     }
 
     @Test
