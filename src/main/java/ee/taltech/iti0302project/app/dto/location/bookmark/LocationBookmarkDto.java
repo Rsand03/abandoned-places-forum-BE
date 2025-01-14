afbf76a6e182913610af6d83c863c3d09cc75c95
mkaasm <mkaasma@gmail.com>
2025-01-14 15:57:24 +0200

    Remove unused import

--- src/main/java/ee/taltech/iti0302project/app/dto/location/bookmark/LocationBookmarkDto.java
+++ src/main/java/ee/taltech/iti0302project/app/dto/location/bookmark/LocationBookmarkDto.java
@@ -2,9 +2,6 @@ package ee.taltech.iti0302project.app.dto.location.bookmark;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
 @Data
 public class LocationBookmarkDto {
     private String type;
